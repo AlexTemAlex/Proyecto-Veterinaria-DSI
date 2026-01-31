e2e2daa9375acc5bf98c656bcd206a5d641e15e7
AlexisCh <temporalxav@outlook.es>
2026-01-31 16:26:54 -0500

    refactor: cambio en la variable global para acceder al webhook de n8n

--- backend/python/api-gateway/main.py
+++ backend/python/api-gateway/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
 # ========================
 # CONFIG
 # ========================
-N8N_BASE_URL = "http://n8n:5678/webhook"
+N8N_BASE_URL = "https://n8n.petsi-dsi.website/webhook"
 
 # ========================
 # MODELS
